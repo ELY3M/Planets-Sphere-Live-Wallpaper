701c20df655fbb66d5fabf3199da5777ca109ee0
ELY M <elymbmx@gmail.com>
2018-10-09 05:17:43 -0500

    stupid permissions

--- app/src/main/java/own/planetsspherelivewallpaper/OpenActivity.java
+++ app/src/main/java/own/planetsspherelivewallpaper/OpenActivity.java
@@ -1,14 +1,20 @@
 package own.planetsspherelivewallpaper;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageButton;
 import android.widget.TextView;
+
+import com.master.permissionhelper.PermissionHelper;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -23,10 +29,38 @@ public class OpenActivity extends Activity {
     Context context;
     String punktestand = "";
     String TAG = "Planets openactivity";
+    PermissionHelper permissionHelper;
 
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         setContentView(R.layout.open);
+
+
+
+        permissionHelper = new PermissionHelper(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 100);
+        permissionHelper.request(new PermissionHelper.PermissionCallback() {
+            @Override
+            public void onPermissionGranted() {
+                Log.d(TAG, "onPermissionGranted() called");
+            }
+
+            @Override
+            public void onIndividualPermissionGranted(String[] grantedPermission) {
+                Log.d(TAG, "onIndividualPermissionGranted() called with: grantedPermission = [" + TextUtils.join(",",grantedPermission) + "]");
+            }
+
+            @Override
+            public void onPermissionDenied() {
+                Log.d(TAG, "onPermissionDenied() called");
+            }
+
+            @Override
+            public void onPermissionDeniedBySystem() {
+                Log.d(TAG, "onPermissionDeniedBySystem() called");
+            }
+        });
+
+
         ((ImageButton) findViewById(R.id.lwpgallery)).setOnClickListener(new OnClickListener() {
             public void onClick(View v) {
                 Intent intent = new Intent();
@@ -114,4 +148,14 @@ public class OpenActivity extends Activity {
         this.punktestand = myscore;
         ((TextView) findViewById(R.id.openscore)).setText(myscore);
     }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (permissionHelper != null) {
+            permissionHelper.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
 }
